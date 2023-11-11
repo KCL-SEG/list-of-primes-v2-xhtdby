31f5e9efba2050f58b949e8f459d707696856fc5
Akshat Dubey <k22013945@kcl.ac.uk>
2023-11-11 16:53:47 +0000

    testing 1

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+from math import log2
 def primes(number_of_primes):
+    reng=2*number_of_primes/log2(number_of_primes)
     list = []
-    return list
+    primes = []
+    for i in (2,reng):
+        list.append(i)
+    primes.append(2)
+    for i in (2,reng):
+        if i in list:
+            primes.append(i)
+            for x in list.copy():
+                if x%i==0:
+                    list.remove(x)
+    return primes[0:number_of_primes]
+
+def __init__():
+    print(primes(100))
